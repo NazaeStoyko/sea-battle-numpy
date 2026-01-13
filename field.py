f024356d34c33c638259d6255208bdfdfed6c8bb
NazaeStoyko <nazarstojko69@gmail.com>
2026-01-13 21:23:50 +0200

    Add fields

---
+++ field.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+field_user = np.zeros(shape=(10,10), dtype=int)
+field_bot = np.zeros(shape=(10,10), dtype=int)
+
+# print(field_user)
+# print('\n')
+# print(field_bot)
+
+# print(field_bot[2][4])
---
+++ ships.py
@@ -0,0 +1,6 @@
+import field as fd
+
+
+x = fd.field_user[1][2] = 4
+
+print(fd.field_user[1][2])
